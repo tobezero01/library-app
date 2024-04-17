d4c42a4dbdc8dd5fb83170f6456a55d4d79098f4
tobezero01 <ducnhuad@gmail.com>
2024-04-17 16:59:27 +0700

    rest-read-only configuration

--- 02-backend/spring-boot-library/src/main/java/com/luvcode/springbootlibrary/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/luvcode/springbootlibrary/dao/BookRepository.java
@@ -1,2 +1,8 @@
-package com.luvcode.springbootlibrary.dao;public class BookRepository {
+package com.luvcode.springbootlibrary.dao;
+
+import com.luvcode.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+
 }
