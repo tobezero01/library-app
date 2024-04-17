29e8ae0ef5fcb41e8379ec645107d135ab601445
tobezero01 <ducnhuad@gmail.com>
2024-04-17 17:09:30 +0700

    setup book model

---
+++ 03-frontend/react-library/src/models/BookModel.ts
@@ -0,0 +1,24 @@
+class BôokModel {
+    id : number;
+    title : string;
+    author ? : string;
+    description ? : string;
+    copies ? : string;
+    copiesAvailable ? : string;
+    category ? : string;
+    img ? : string;
+
+    constructor(id : number, title : string, author : string, description : string, 
+        copies : string ,copiesAvailable  : string, category : string, img : string) {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.description = description;
+            this.copies = copies;
+            this.copiesAvailable = copiesAvailable;
+            this.category = category;
+            this.img = img;
+        
+    }
+    
+}
