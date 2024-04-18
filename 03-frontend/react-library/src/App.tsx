4530c43bce651e44153535c76b60896dc9232c9f
tobezero01 <ducnhuad@gmail.com>
2024-04-18 10:50:18 +0700

    router and switch

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,14 +4,26 @@ import { NavBar } from './layout/NavBarAndFooter/NavBar';
 import { Footer } from './layout/NavBarAndFooter/footer';
 import { Homepage } from './layout/HomePage/Homepage';
 import { SearchBookPage } from './layout/SearchBookPage/SearchBookPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 
 export const App =() => {
   return (
     <div>
       <NavBar />
-      {/* <Homepage/> */}
-      <SearchBookPage/>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home'></Redirect>
+        </Route>
+        <Route path='/home' exact>
+          <Homepage />
+        </Route>
+        <Route path='/search'>
+          <SearchBookPage />
+        </Route>
+      </Switch>
+      
+      
       <Footer/>
     </div>
     
