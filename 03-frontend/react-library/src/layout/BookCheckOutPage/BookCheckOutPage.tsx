be8f49313a9fb1aafd774b11d5af909046096e78
tobezero01 <ducnhuad@gmail.com>
2024-04-18 12:02:55 +0700

    checkout view and link view detail

--- 03-frontend/react-library/src/layout/BookCheckOutPage/BookCheckOutPage.tsx
+++ 03-frontend/react-library/src/layout/BookCheckOutPage/BookCheckOutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { isBoolean } from "util";
 
 export const BookCheckOutPage = () => {
     const [book, setBook] = useState<BookModel>();
@@ -58,6 +59,50 @@ export const BookCheckOutPage = () => {
     }
     return (
         <div>
+            <div className="container d-none d-lg-block">
+                <div className="row mt-5">
+                    <div className="col-sm-2 col-md-2">
+                        {book?.img ?
+                            <img src={book?.img} width='226' height='349' alt='Book' />
+                            :
+                            <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} 
+                                width='226'
+                                height='349' 
+                                alt='Book' />
+                        }
+                    </div>
+                    <div className="col-4 col-md-4 container">
+                        <div className="ml-2">
+                            <h2>{book?.title}</h2>
+                            <h5 className="text-primary">{book?.author}</h5>
+                            <p className="lead">{book?.description}</p>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+
+            </div>
+            <div className="container d-lg-none mt-5">
+                <div className="d-flex justify-content-center">
+                    {book?.img ?
+                        <img src={book?.img} width='226' height='349' alt='Book' />
+                        :
+                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} 
+                            width='226'
+                            height='349' 
+                            alt='Book' />
+                    }
+                </div>
+                <div className="mt-4">
+                    <div className="ml-2">
+                        <h2>{book?.title}</h2>
+                        <h5 className="text-primary">{book?.author}</h5>
+                        <p className="lead">{book?.description}</p>
+
+                    </div>
+                </div>
+                <hr />
+            </div>
 
         </div>
     );
