a2dec86ad6510ddbe1d071e44bc0e82eaac3c05d
tobezero01 <ducnhuad@gmail.com>
2024-05-05 14:01:50 +0700

    Book checkout useEffect

--- 03-frontend/react-library/src/layout/BookCheckOutPage/BookCheckOutPage.tsx
+++ 03-frontend/react-library/src/layout/BookCheckOutPage/BookCheckOutPage.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react"
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
-import { isBoolean } from "util";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
-import ReviewModel from "../../models/ReviewModel";
 import { LatestReviews } from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
-import { Authenticator } from "@okta/okta-auth-js/lib/idx/authenticator";
 
 
 export const BookCheckOutPage = () => {
@@ -24,18 +21,10 @@ export const BookCheckOutPage = () => {
     const [totalStars, setTotalStars] = useState(0);
     const [isloadingReview, setIsLoadingReview] = useState(true);
 
-    // loan count state
-    const [currentLoansCount, setCurrentLoansCount] = useState(0);
-    const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState (true);
 
 
-    // is book checked out ?
-    const [isCheckedOut, setIsCheckedOut] = useState(false);
-    const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
-    
     const bookId = (window.location.pathname).split("/")[2];
 
-    // lấy dữ liệu từ backend
     useEffect(() => {
         const fetchBook = async() => {
             const baseUrl :string = `http://localhost:8080/api/books/${bookId}`;
@@ -110,35 +99,6 @@ export const BookCheckOutPage = () => {
         })
     },[]);
 
-    useEffect(() =>	{
-        const fetchUserCurrentLoansCount = async () => {
-            if(authState && authState.isAuthenticated) {
-                const url = `https://localhost:8080/api/books/secure/currentLoans.count`
-                const requestOptions = {
-                    method : 'GET',
-                    headers :{
-                        Authorization : `Bearer ${authState.accessToken?.accessToken}`,
-                        'Content-type': 'application/json'
-                    }
-                };
-                const currentLoansCountResponse = await fetch(url, requestOptions);
-                if(!currentLoansCountResponse.ok) {
-                    throw new Error("Something went wrong!");
-                }
-
-                const currentLoansCountResponseJson = await currentLoansCountResponse.json();
-                setCurrentLoansCount(currentLoansCountResponseJson);
-            }
-            setIsLoadingCurrentLoansCount(false);
-        }
-
-        fetchUserCurrentLoansCount().catch((error : any) => {
-            setIsLoadingCurrentLoansCount(false);
-            setHttpError(error.message);
-        })
-
-    }, [authState]);
-
     useEffect(() => {
         const fetchUserCurrentLoansCount = async () => {
             if (authState && authState.isAuthenticated) {
@@ -201,7 +161,6 @@ export const BookCheckOutPage = () => {
                             <StarsReview rating={totalStars} size={32} />
                         </div>
                     </div>
-                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}/>
                     {/* <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
                         isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
                         checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/> */}
@@ -231,11 +190,9 @@ export const BookCheckOutPage = () => {
                         <StarsReview rating={totalStars} size={32} />
                     </div>
                 </div>
-                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}/>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
-
         </div>
     );
 }
