419193360422aa79f2d67bdca7783f9c8fd1804a
tobezero01 <ducnhuad@gmail.com>
2024-05-07 15:45:49 +0700

    increase mapping controller

--- 02-backend/spring-boot-library/src/main/java/com/luvcode/springbootlibrary/controller/AdminController.java
+++ 02-backend/spring-boot-library/src/main/java/com/luvcode/springbootlibrary/controller/AdminController.java
@@ -18,15 +18,15 @@ public class AdminController {
         this.adminService = adminService;
     }
 //
-//    @PutMapping("/secure/increase/book/quantity")
-//    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
-//                                     @RequestParam Long bookId) throws Exception {
-//        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
-//        if (admin == null || !admin.equals("admin")) {
-//            throw new Exception("Administration page only");
-//        }
-//        adminService.increaseBookQuantity(bookId);
-//    }
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
 //
 //    @PutMapping("/secure/decrease/book/quantity")
 //    public void decreaseBookQuantity(@RequestHeader(value="Authorization") String token,
